5d3d9ee414e1e44e958dccaf59e017d9ce3c7cbc
Анна Шестакова <annanoway7@gmail.com>
2022-07-23 16:24:29 +0300

    mole-game done

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,25 @@
+let hole = document.getElementsByClassName('hole');
+let dead = document.getElementById('dead');
+let lost = document.getElementById('lost');
+
+for(let index = 1; index < 10; index++) {
+    const hole = document.getElementById(`hole${index}`)
+    hole.onclick = () => {
+      if(hole.className.includes('hole_has-mole')) {
+        dead.textContent++;
+      } else {
+        lost.textContent++;
+      }
+
+      if(dead.textContent >= 10) {
+        alert('Вы победили!');
+        dead.textContent = 0;
+        lost.textContent = 0;
+      } else if(lost.textContent >= 5) {
+        alert('Вы проиграли!');
+        dead.textContent = 0;
+        lost.textContent = 0;
+      }
+      
+    }
+  }
