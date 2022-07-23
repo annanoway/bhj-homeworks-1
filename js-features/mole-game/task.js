24a0f0c507d07aa4aeb4acb0f21fff99ce201d51
Анна Шестакова <annanoway7@gmail.com>
2022-07-23 16:30:10 +0300

    mole-game v2

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -11,15 +11,14 @@ for(let index = 1; index < 10; index++) {
         lost.textContent++;
       }
 
-      if(dead.textContent >= 10) {
+      if(dead.textContent === '10') {
         alert('Вы победили!');
         dead.textContent = 0;
         lost.textContent = 0;
-      } else if(lost.textContent >= 5) {
+      } else if(lost.textContent === '5') {
         alert('Вы проиграли!');
         dead.textContent = 0;
         lost.textContent = 0;
       }
-      
     }
   }
