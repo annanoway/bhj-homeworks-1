a57cb74a98903a5313f6e62dc37cc1af401176df
Анна Шестакова <annanoway7@gmail.com>
2022-08-06 15:50:54 +0300

    keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    document.addEventListener('keydown', (event) => {
+      let currentChar = this.currentSymbol.textContent;
+        if (event.key === currentChar) {
+          this.success();
+        } else {
+          this.fail();
+        }
+    });
   }
 
   success() {
